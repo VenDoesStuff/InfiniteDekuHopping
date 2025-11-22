4f536178947b6c6aa7ba0c4cf6c13b481804b8ea
VenDoesStuff <VentusDoesStuff@gmail.com>
2025-11-22 09:32:51 -0700

    doubled minimum hopping speed, made it so A stops counter decreasing instead of setting it to 5

--- src/InfiniteHops.c
+++ src/InfiniteHops.c
@@ -2,10 +2,22 @@
 #include "global.h"
 #include "recomputils.h"
 #include "recompconfig.h"
+#include "z64player.h"
 
 RECOMP_HOOK ("func_808373F8") void InfiniteHops(PlayState* play, Player* this, u16 sfxId) {
+    f32 speed;
+    f32 speedXZ;
+    // Make it so holding A stops hops counter from decreasing by raising it
     if (CHECK_BTN_ANY(CONTROLLER1(&play->state)->cur.button, BTN_A)){
-        this->remainingHopsCounter = 5;}
+        this->remainingHopsCounter++;
+    }
+    if (this->remainingHopsCounter > 5){
+        this->remainingHopsCounter = 5;
+    }
 
+    //Raise minimum hopping speed
+        if (this->speedXZ < 8.0f) {
+            this->speedXZ = 8.0f;
+    }
     
 }
